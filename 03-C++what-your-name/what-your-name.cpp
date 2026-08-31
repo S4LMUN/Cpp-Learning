07fa067914e814264cda886710d2bc42cf5673d0
S4LMUN <chayutpong2154@gmail.com>
2026-08-31 19:29:25 +0700

    feat(what-your-name): what your full name?

--- 03-C++what-your-name/what-your-name.cpp
+++ 03-C++what-your-name/what-your-name.cpp
@@ -3,16 +3,22 @@
 
 int main()
 {
-	std::string name;
+	std::string fullname;
+	std::string nickname;
 	int age;
 
-	std::cout << "What's your name? : ";
-	std::cin >> name;
+	std::cout << "What's your full name : ";
+	std::getline(std::cin, fullname);
+
+	std::cout << "What's your nick name? : ";
+	std::cin >> nickname;
 
 	std::cout << "What your age? : ";
 	std::cin >> age;
 
-	std::cout << "Hello " << name << '\n';
+	std::cout << "Ohh you is " << fullname << " right?" << '\n';
+
+	std::cout << "Nice to see you " << nickname << '\n';
 	std::cout << "You are " << age << " years old" << '\n';
 
 	return 0;
